c1075ffa34e25c3ccf8ed8683616d57e5afacb1a
Gustavo Flores <gustavo.sfq@gmail.com>
2021-03-10 23:57:34 -0300

    set migrate & post api

--- server/routes/post.js
+++ server/routes/post.js
@@ -1,26 +1,45 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+
+const { Post } = require("../models/index");
 
 /* GET home page. */
-router.get('/posts', function(req, res, next) {
-  res.json({
-    hola: "mundo"
-  });
+router.get("/posts", async (req, res, next) => {
+    try {
+        const posts = await Post.findAll();
+        res.json(posts);
+    } catch (error) {
+        next(error);
+    }
 });
-router.get('/posts/:postId', function(req, res, next) {
-  res.json({
-    hola: "mundo"
-  });
+
+router.post("/posts", async (req, res, next) => {
+    const { name, description } = req.body;
+    if (!name || name.lenght < 2)
+        throw "Debes incluir name o debe tener mas de 2 caracteres";
+    if (!description || description.lenght < 2)
+        throw "Debes incluir description o debe tener mas de 2 caracteres";
+
+    try {
+        const post = await Post.create({
+            name,
+            description,
+        });
+        res.json(post);
+    } catch (error) {
+        next(error);
+    }
 });
-router.put('/posts/:postId', function(req, res, next) {
-  res.json({
-    hola: "mundo"
-  });
-});
-router.delete('/posts/:postId', function(req, res, next) {
-  res.json({
-    hola: "mundo"
-  });
+router.delete("/posts/:postId", async (req, res, next) => {
+    const { postId } = req.params;
+    if (!postId) throw "Debes incluir postId";
+    try {
+        const post = await Post.findOne({where:{id:postId}});
+        await post.destroy();
+        res.json(post);
+    } catch (error) {
+        next(error);
+    }
 });
 
 module.exports = router;
